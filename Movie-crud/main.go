7e393b96d98031a4bad82400c1d12216366d1a3a
ishansaini194 <ishan.saini880@gmail.com>
2026-01-12 14:12:31 +0530

    Movie-CRUD
    Signed-off-by: ishansaini194 <ishan.saini880@gmail.com>

---
+++ Movie-crud/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	Id       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.Id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+		}
+	}
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.Id == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{Id: "1", Isbn: "401", Title: "Batman", Director: &Director{Firstname: "Matt", Lastname: "Reeves"}})
+	movies = append(movies, Movie{Id: "2", Isbn: "402", Title: "Kantara", Director: &Director{Firstname: "Rishab", Lastname: "Shetty"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server on port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
