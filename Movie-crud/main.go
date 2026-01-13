6d8ffd2d9f609884ec78c70746f1b26ef84061fc
ishansaini194 <ishan.saini880@gmail.com>
2026-01-13 20:05:18 +0530

    final updates
    Signed-off-by: ishansaini194 <ishan.saini880@gmail.com>

--- Movie-crud/main.go
+++ Movie-crud/main.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -54,6 +56,7 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.Id = strconv.Itoa(rand.Intn(1000000))
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
 }
