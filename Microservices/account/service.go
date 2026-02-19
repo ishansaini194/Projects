71bc9d8835dc3907ace5673ee0019cc23067e97d
ishansaini194 <ishan.saini880@gmail.com>
2026-02-19 23:43:53 +0530

    account

---
+++ Microservices/account/service.go
@@ -0,0 +1,18 @@
+package account
+
+import "context"
+
+type Service interface {
+	PostAccont(ctx context.Context, name string) (*Account, error)
+	GetAccount(ctx context.Context, id string) (*Account, error)
+	GetAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error)
+}
+
+type Account struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type accountService struct {
+	repository Repository
+}
