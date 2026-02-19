29ce19e6ba8f7999e08c81334a913f23f75e3d4a
ishansaini194 <ishan.saini880@gmail.com>
2026-02-20 01:01:37 +0530

    main.go

--- Microservices/account/service.go
+++ Microservices/account/service.go
@@ -1,6 +1,10 @@
 package account
 
-import "context"
+import (
+	"context"
+
+	"github.com/segmentio/ksuid"
+)
 
 type Service interface {
 	PostAccont(ctx context.Context, name string) (*Account, error)
@@ -15,4 +19,33 @@ type Account struct {
 
 type accountService struct {
 	repository Repository
+}
+
+func newService(r Repository) Service {
+	return &accountService{r}
+}
+
+func (s *accountService) PostAccont(ctx context.Context, name string) (*Account, error) {
+	a := &Account{
+		Name: name,
+		ID:   ksuid.New().String(),
+	}
+
+	if err := s.repository.PutAccount(ctx, *a); err != nil {
+		return nil, err
+	}
+
+	return a, nil
+}
+
+func (s *accountService) GetAccount(ctx context.Context, id string) (*Account, error) {
+	return s.repository.GetAccountByID(ctx, id)
+}
+
+func (s *accountService) GetAccounts(ctx context.Context, skip uint64, take uint64) ([]Account, error) {
+	if take > 100 || (skip == 0 && take == 0) {
+		take = 100
+	}
+	return s.repository.ListAccounts(ctx, skip, take)
+
 }
--- Microservices/go.mod
+++ Microservices/go.mod
@@ -5,7 +5,9 @@ go 1.25.6
 require (
 	github.com/99designs/gqlgen v0.17.86
 	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/segmentio/ksuid v1.0.4
 	github.com/vektah/gqlparser/v2 v2.5.31
+	google.golang.org/grpc v1.79.1
 )
 
 require (
@@ -15,4 +17,9 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 )
